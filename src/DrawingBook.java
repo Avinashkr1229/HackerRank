deb923646e194c6b96c935455bd532242220051d
Avinashkr1229 <shiva.avi1229@gmail.com>
2022-08-14 19:48:20 +0530

    New Solution

---
+++ src/DrawingBook.java
@@ -0,0 +1,12 @@
+public class DrawingBook {
+    public static int pageCount(int n, int p) {
+        int k = p / 2;
+        int m = n % 2 == 0 ? (n - p + 1) / 2 : (n - p) / 2;
+        return Math.min(k, m);
+
+    }
+
+    public static void main(String[] args) {
+        pageCount(6, 2);
+    }
+}
