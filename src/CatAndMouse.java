a5d7904ee16b48d9b736aed69792ee4bd7c3f437
Avinashkr1229 <shiva.avi1229@gmail.com>
2022-08-14 13:12:01 +0530

    Add existing project files to Git

---
+++ src/CatAndMouse.java
@@ -0,0 +1,32 @@
+public class CatAndMouse {
+    static String catAndMouseSolution(int x, int y, int z) {
+        String res = "";
+        try {
+            if (z < x) {
+                x = x - z;
+            } else {
+                x = z - x;
+            }
+
+            if (z < y) {
+                y = y - z;
+            } else {
+                y = z - y;
+            }
+            System.out.println(x+" "+y);
+            if (x < y) {
+                res = "Cat A";
+            } else if (x == y) {
+                res = "Mouse C";
+            } else
+                res = "Mouse B";
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        catAndMouseSolution(1, 2, 3);
+    }
+}
