17bd89068ad32aa1ba5c22ce2120b84004900689
Avinashkr1229 <shiva.avi1229@gmail.com>
2022-08-14 13:14:40 +0530

    Modified

--- src/CatAndMouse.java
+++ src/CatAndMouse.java
@@ -1,6 +1,7 @@
 public class CatAndMouse {
     static String catAndMouseSolution(int x, int y, int z) {
         String res = "";
+        System.out.println("hi");
         try {
             if (z < x) {
                 x = x - z;
