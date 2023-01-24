dc4b3a63cd0e0aa5173ffd2b892648b76f4b2412
shiva <shiva.avi1229@gmail.com>
2023-01-24 17:59:09 +0530

    New Leetcode problem

--- src/MaxSubArraySum.java
+++ src/MaxSubArraySum.java
@@ -1,16 +1,15 @@
 public class MaxSubArraySum {
     public static int maxSubArray(int[] nums) {
-        int max_so_far = Integer.MIN_VALUE, max_ending_here
-                = 0;
+        int start = Integer.MIN_VALUE, sum = 0;
 
         for (int num : nums) {
-            max_ending_here = max_ending_here + num;
-            if (max_so_far < max_ending_here)
-                max_so_far = max_ending_here;
-            if (max_ending_here < 0)
-                max_ending_here = 0;
+            sum = sum + num;
+            if (start < sum)
+                start = sum;
+            if (sum < 0)
+                sum = 0;
         }
-        return max_so_far;
+        return start;
     }
 
     public static void main(String[] args) {
