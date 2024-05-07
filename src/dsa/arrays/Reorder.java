cc7cdbca96e78531e718434f9cc810e6e53563b9
shiva <shiva.avi1229@gmail.com>
2024-05-07 22:07:24 +0530

    DSA Array problem: Day10 Reorder

--- src/dsa/arrays/Reorder.java
+++ src/dsa/arrays/Reorder.java
@@ -3,19 +3,24 @@ package dsa.arrays;
 public class Reorder {
 
 
-    static void reorder(int[] arr, int[] index, int n){
+    /*
+    *
+    * Time Complexity: O(n)
+      Auxiliary Space: O(n)
+* */
+    static void reorder(int[] arr, int[] index, int n) {
 
         //using auxilary array
         int[] tempArray = arr.clone();
-        for(int i=0;i<index.length;i++){
-            arr[index[i]]=tempArray[i];
+        for (int i = 0; i < index.length; i++) {
+            arr[index[i]] = tempArray[i];
         }
 
         //cyclic sort
-        int k=0;
-        while(k<n){
+        int k = 0;
+        while (k < n) {
             int correct = index[k];
-            if(index[correct]!=index[k])
+            if (index[correct] != index[k])
                 swap(index, correct, k);
             else
                 k++;
@@ -23,14 +28,15 @@ public class Reorder {
 
     }
 
-    static void swap(int[] index, int left, int right){
+    static void swap(int[] index, int left, int right) {
         int temp = index[left];
-        index[left]=index[right];
-        index[right]=temp;
+        index[left] = index[right];
+        index[right] = temp;
     }
+
     public static void main(String[] args) {
         int[] arr = {50, 40, 70, 60, 90};
-        int[] index = {3,  0,  4,  1,  2};
+        int[] index = {3, 0, 4, 1, 2};
         reorder(arr, index, arr.length);
     }
 }
